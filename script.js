56b1652eff011251175f1495d677ea2bec81ae46
shivamsontakke321-create <shivamsontakke321@gmail.com>
2026-08-28 16:48:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showSurprise() {
+    document.getElementById("surprise").innerHTML = `
+        <div class="surprise-message">
+            💖 You are one of the most special people in my life! 💖
+            <br><br>
+            I hope your birthday is filled with happiness,
+            laughter, love and beautiful memories. 🎂✨
+            <br><br>
+            May all your dreams come true. 🌸
+            <br>
+            Happy Birthday Toto! 🎉❤️
+        </div>
+    `;
+}
